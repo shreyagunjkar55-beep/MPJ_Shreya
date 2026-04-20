f216f1d100524352a908317165e8baf7b429bbf4
Shreya Gunjkar <shreyagunjkar55@gmail.com>
2026-04-20 18:03:01 +0530

    Add files via upload

---
+++ Lab5/DBConnection.java
@@ -0,0 +1,9 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/employee_db", "root", "root");
+    }
+}
